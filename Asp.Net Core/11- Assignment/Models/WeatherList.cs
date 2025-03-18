26f700246a310295a1ef6734b30db8cfc3b1a523
Keythai <tcyyap@gmail.com>
2025-03-19 01:13:35 +0800

    Add files via upload

---
+++ Asp.Net Core/11- Assignment/Models/WeatherList.cs
@@ -0,0 +1,7 @@
+﻿namespace Assignment.Models
+{
+    public class WeatherList
+    {
+        public List<CityWeather> weathers { get; set; } = new List<CityWeather>();
+    }
+}
---
+++ Asp.Net Core/11- Assignment/ViewComponents/CityViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Assignment.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment.ViewComponents
+{
+    public class CityViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(CityWeather grid)
+        {
+            return View("Default", grid);
+        }
+    }
+}
