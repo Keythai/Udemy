3f0608ad3774a6b8ef9f7eaa06c8718b239fdb82
Keythai <tcyyap@gmail.com>
2025-04-10 21:59:38 +0800

    Updated 19 - Advanced Unit Testing

---
+++ Asp.Net Core/Courses/18 - EFCore/CRUDExample/Controllers/CountriesController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServiceContracts;
+
+namespace CRUDExample.Controllers
+{
+    [Route("[controller]")]
+    public class CountriesController : Controller
+    {
+        private readonly ICountriesService _countriesService;
+        public CountriesController(ICountriesService countriesService)
+        {
+            _countriesService = countriesService;
+        }
+        [Route("UploadFromExcel")]
+        public IActionResult UploadFromExcel()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadFromExcel(IFormFile excelFile)
+        {
+            if (excelFile == null || excelFile.Length == 0)
+            {
+                ViewBag.ErrorMessage = "Please select a xlsx file to upload.";
+                return View();
+            }
+            if(!Path.GetExtension(excelFile.FileName).Equals(".xlsx", StringComparison.OrdinalIgnoreCase))
+            {
+                ViewBag.ErrorMessage = "Unsupported file! Please select a valid xlsx file.";
+                return View();
+            }
+
+            int countriesCountInserted = await _countriesService.UploadCountriesFromExcelFile(excelFile);
+            ViewBag.Message = $"{countriesCountInserted} countries were added to the database.";
+            return View();
+        }
+    }
+}
