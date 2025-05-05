c9cf9b55c69b69c9bc5834b50402c5340da832e0
Keythai <tcyyap@gmail.com>
2025-05-06 07:06:50 +0800

    Added course 27

---
+++ Asp.Net Core/Assignments/26 - Assignment/OrderAPI/OrderAPI/Controllers/ErrorController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OrderAPI.Controllers
+{
+    [Route("error")]
+    [ApiController]
+    public class ErrorController : ControllerBase
+    {
+        [HttpGet("")]
+        public IActionResult Error()
+        {
+            var context = HttpContext.Features.Get<IExceptionHandlerFeature>();
+            var exception = context.Error;
+            return Problem(
+                detail: exception.Message,
+                statusCode: StatusCodes.Status500InternalServerError,
+                title: "An error occurred while processing your request."
+            );
+        }
+    }
+}
